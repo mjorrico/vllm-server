f83f4fce9a1fd3a0b673abb75c8f6884e6b6ee79
mjorrico <jordanenrico214@gmail.com>
2025-12-15 04:49:36 +0700

    update: changed initialization query

--- clickhouse_config/init_database.sql
+++ clickhouse_config/init_database.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS vllm_logger.vllm_log (
     memory_used Int64,
     memory_total Int64,
     temperature Nullable(Float64)
-) ENGINE = MergeTree()
+) 
+ENGINE = MergeTree()
 PARTITION BY toYYYYMMDD(created_at)
-ORDER BY (created_at DESC, gpu_index)
-PRIMARY KEY (created_at, gpu_index);
+ORDER BY (created_at, gpu_index);
