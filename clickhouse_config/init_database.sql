017ab21eccc24447ac730789495cb09ce3748b43
mjorrico <jordanenrico214@gmail.com>
2025-12-14 23:48:50 +0700

    update: completed init database for clickhouse!

---
+++ clickhouse_config/init_database.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS vllm_log (
+    created_at DateTime64(3),
+    gpu_index Int32,
+    gpu_utilization Float64,
+    memory_used Int64,
+    memory_total Int64,
+    temperature Nullable(Float64)
+) ENGINE = MergeTree()
+PARTITION BY toYYYYMMDD(created_at)
+ORDER BY (created_at DESC, gpu_index)
+PRIMARY KEY (created_at, gpu_index);
