f959aac357a256bd837be4a23f7e4daf2468ce3a
mjorrico <jordanenrico214@gmail.com>
2025-12-20 01:33:07 +0700

    fix: /metrics query is fixed

--- backend/app/api/routers/metrics.py
+++ backend/app/api/routers/metrics.py
@@ -11,14 +11,12 @@ async def get_metrics():
         port=8123,
     )
     # queries to be run
-    query = """
-        SELECT 
-            toDate(created_at) AS day, 
-            round(avg(gpu_utilization), 2) AS avg_gpu_util_percent, 
-            round(avg(memory_used / memory_total) * 100, 2) AS avg_mem_util_percent
-        FROM vllm_log
-        GROUP BY day
-        ORDER BY day DESC;
-    """
+    query = """SELECT 
+    toDate(created_at) AS day, 
+    round(avg(gpu_utilization), 2) AS avg_gpu_util_percent, 
+    round(avg(memory_used / memory_total) * 100, 2) AS avg_mem_util_percent
+FROM vllm_log
+GROUP BY day
+ORDER BY day DESC"""
     result = await db.run_query(query)
     return result
